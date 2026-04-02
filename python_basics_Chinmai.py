9e69c59f69c7c455e515f747ed5cc80987f87705
Chinmai2408 <chinmainagisetty@gmail.com>
2026-04-02 10:34:20 +0530

    Assignment

---
+++ python_basics_Chinmai.py
@@ -0,0 +1,13 @@
+#Task1
+person_name="Ncsc"
+age=20
+height=156
+is_student=True
+print("Name:",person_name,",","Age:",age,"years old,","Height:",height,"m,","is_student:",is_student)
+#Task2
+print(f"Name:{person_name} , Age: {age} years old, Height: {height} m, is_student: {is_student}")
+#Task3
+print("Age in months:",age*12)
+print("Age in days:",age*12*365)
+print("Remainder when age is divided by 7:",age%7)
+print("Age Power 2:",age**2)
